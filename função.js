659805e3e2f41149dace74d90a692d23ac4a1898
Jônathasds <jonathadelgado@gmail.com>
2022-05-17 07:35:04 -0300

    comentando codigo da calculadora

--- função.js
+++ função.js
@@ -1,32 +1,32 @@
     function calcular(){
-        const n1 = Number(document.getElementById("num1").value);
-        const n2 =Number(document.getElementById("num2").value);
+        const n1 = Number(document.getElementById("num1").value); //pegando o valor do input num1
+        const n2 =Number(document.getElementById("num2").value); // pegando o valor do input num2
 
         //const n1 = num1.value;
        // const n2 = num2.value;
 
 
-        let operador = document.getElementById("operação").value;
-        const result = document.getElementById('result');
+        const operador = document.getElementById("operação").value; //pegando o valor do sinal
+        const result = document.getElementById('result');  //pegando a tag "p" 
 
     //let operador = operação.value;
     
-    if ( operador == "+"){
-        var s = n1 + n2
+    if ( operador == "+"){ // dando 1° condição mediante a escolha do usuario
+        let s = n1 + n2
 
-        result.innerHTML = ` a soma de ${n1} + ${n2} é igual a ${s}! `
-    
+        result.innerHTML = ` a soma de ${n1} + ${n2} é igual a ${s}! ` //2° condição
+    //result.innerHTML é o comando pra imprimir o resultado na tag "p" no console.
     }else if ( operador == "-"){
-        var s = n1 - n2
-        result.innerHTML = `a subtração de ${n1} - ${n2} é igual a ${s}!`
+        let s = n1 - n2
+        result.innerHTML = `a subtração de ${n1} - ${n2} é igual a ${s}!` //3° condição
     
     }else if (operador == "/"){
-        var s = n1 / n2
-        result.innerHTML = `a divisão de ${n1} / ${n2} é igual a ${s}`
+        let s = n1 / n2
+        result.innerHTML = `a divisão de ${n1} / ${n2} é igual a ${s}` // 4° condição
     
     }else {
-        var s = n1 * n2
-        result.innerHTML = ` a multiplicação de ${n1} X ${n2} é igual a ${s}`
+        let s = n1 * n2
+        result.innerHTML = ` a multiplicação de ${n1} X ${n2} é igual a ${s}`// na ultima não precisei da condição, se a escolhida pelo usuario não for nenhuma à cima, entrarar nessa.
     }
 
     
